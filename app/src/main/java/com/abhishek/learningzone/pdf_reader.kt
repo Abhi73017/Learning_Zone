0d9044beeeaeac83c2c852d0d12d4e5f5cc274d7
aryanvikash <followvikash8@gmail.com>
2020-04-22 17:15:47 +0530

    pdf view fixed

--- app/src/main/java/com/abhishek/learningzone/pdf_reader.kt
+++ app/src/main/java/com/abhishek/learningzone/pdf_reader.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_pdf_reader.*
+import java.io.UnsupportedEncodingException
+import java.net.URLEncoder
 
 class pdf_reader : AppCompatActivity() {
 
@@ -16,10 +18,15 @@ class pdf_reader : AppCompatActivity() {
         webView.settings.setSupportZoom(true)
         webView.settings.javaScriptEnabled = true
 
-        //val pdflink= intent.getStringExtra("downloaduri") ?: return
+        var pdflink= intent.getStringExtra("downloaduri") ?: return
 
-        val pdflink = "https://mindorks.s3.ap-south-1.amazonaws.com/courses/MindOrks_Android_Online_Professional_Course-Syllabus.pdf"
+//        var pdflink = "https://mindorks.s3.ap-south-1.amazonaws.com/courses/MindOrks_Android_Online_Professional_Course-Syllabus.pdf"
+        try {
+            pdflink = URLEncoder.encode(pdflink, "UTF-8")
+        } catch (e: UnsupportedEncodingException) {
+            e.printStackTrace()
+        }
+        webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=$pdflink")
 
-        webView.loadUrl("https://docs.google.com/gview?embedded=true&url=$pdflink")
     }
 }
